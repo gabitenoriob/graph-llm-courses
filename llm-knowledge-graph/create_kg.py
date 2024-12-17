9c0140ab2c02dcad3e55906bb996861a5e7cdc0e
Gabriela Batista Tenorio das Neves <gabriela.neves@cgu.gov.br>
2024-12-17 08:10:01 -0300

    create and query

--- llm-knowledge-graph/create_kg.py
+++ llm-knowledge-graph/create_kg.py
@@ -12,8 +12,10 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from dotenv import load_dotenv
 load_dotenv()
 
+#Entrada
 DOCS_PATH = "llm-knowledge-graph\\data\\dados"
 
+#Configurando LLM
 llm = ChatOpenAI(
     openai_api_base="http://localhost:1234/v1",  # URL do servidor API do LM Studio
     model_name="multi-qa-MiniLM-L6-cos-v1",  # Nome do modelo configurado no LM Studio
@@ -21,16 +23,18 @@ llm = ChatOpenAI(
     temperature=0.0,  
 )
 
-# Fazer uma consulta ao modelo
+# Fazer uma consulta ao modelo como teste
 response = llm.predict("Explique a importância dos embeddings no NLP.")
 print(response)
 
+#Configurando provedor de embeddings
 embedding_provider = OpenAIEmbeddings(
     openai_api_base="http://localhost:1234/v1",  # URL base do LM Studio
     model="multi-qa-MiniLM-L6-cos-v1",  # Substitua pelo modelo configurado no LM Studio
     openai_api_key=os.getenv('OPENAI_API_KEY'),  # Não usado, mas necessário
 )
 
+#Alternativa 
 # embedding_provider = HuggingFaceEmbeddings(
 #     model_name="multi-qa-MiniLM-L6-cos-v1",
 #     model_kwargs={"device": "cpu"}  # ou "cuda" se você estiver usando GPU
@@ -43,19 +47,23 @@ vector = embedding_provider.embed_query(query)
 print(vector)
 
 
+#Conexão com o BD
 graph = Neo4jGraph(
     url=os.getenv('NEO4J_URI'),
     username=os.getenv('NEO4J_USERNAME'),
     password=os.getenv('NEO4J_PASSWORD')
 )
 
+#Cria um transformador de grafo que usará o LLM para extrair entidades e relacionamentos do texto.
 doc_transformer = LLMGraphTransformer(
     llm=llm,
     )
 
-# Load and split the documents
+
+#Carrega arquivos PDF da pasta data usando o DirectoryLoader.
 loader = DirectoryLoader(DOCS_PATH, glob="**/*.pdf", loader_cls=PyPDFLoader)
 
+#Divide o texto dos PDFs em chunks (blocos) menores usando o CharacterTextSplitter.
 text_splitter = CharacterTextSplitter(
     separator="\n\n",
     chunk_size=1500,
@@ -65,6 +73,8 @@ text_splitter = CharacterTextSplitter(
 docs = loader.load()
 chunks = text_splitter.split_documents(docs)
 
+
+#Para cada chunk gera identificadores,cria embeddings,armazena o chunk no Neo4j
 for chunk in chunks:
 
     filename = os.path.basename(chunk.metadata["source"])
@@ -93,10 +103,10 @@ for chunk in chunks:
         properties
     )
 
-    # Generate the entities and relationships from the chunk
+    #Extrai entidades e relacionamentos usando o LLM:
     graph_docs = doc_transformer.convert_to_graph_documents([chunk])
 
-    # Map the entities in the graph documents to the chunk node
+    #Relaciona entidades ao chunk
     for graph_doc in graph_docs:
         chunk_node = Node(
             id=chunk_id,
@@ -116,7 +126,8 @@ for chunk in chunks:
     # add the graph documents to the graph
     graph.add_graph_documents(graph_docs)
 
-# Create the vector index
+#Cria um índice vetorial chamado chunkVector no Neo4j para nós Chunk
+#Configura a similaridade vetorial como cosine com dimensões 384 (compatível com os embeddings gerados pelo modelo multi-qa-MiniLM-L6-cos-v1).
 graph.query("""
     CREATE VECTOR INDEX `chunkVector`
     IF NOT EXISTS
--- llm-knowledge-graph/query_kg.py
+++ llm-knowledge-graph/query_kg.py
@@ -7,6 +7,7 @@ from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 load_dotenv()
 
+#Inicializa LLM
 llm = ChatOpenAI(
     openai_api_base="http://localhost:1234/v1",  # URL do servidor API do LM Studio
     model_name="multi-qa-MiniLM-L6-cos-v1",  # Nome do modelo configurado no LM Studio
@@ -14,12 +15,14 @@ llm = ChatOpenAI(
     temperature=0.0,  
 )
 
+#Conexão com BD
 graph = Neo4jGraph(
     url=os.getenv('NEO4J_URI'),
     username=os.getenv('NEO4J_USERNAME'),
     password=os.getenv('NEO4J_PASSWORD')
 )
 
+#Configuração do prompt para geração de Cypher
 CYPHER_GENERATION_TEMPLATE = """Task:Generate Cypher statement to query a graph database.
 Instructions:
 Use only the provided relationship types and properties in the schema.
@@ -34,22 +37,29 @@ Schema:
 The question is:
 {question}"""
 
+#O PromptTemplate organiza as variáveis schema (estrutura do banco de dados) e question (pergunta em linguagem natural) para gerar o prompt que será usado pelo modelo.
 cypher_generation_prompt = PromptTemplate(
     template=CYPHER_GENERATION_TEMPLATE,
     input_variables=["schema", "question"],
 )
 
+#Inicializa o GraphCypherQAChain
 cypher_chain = GraphCypherQAChain.from_llm(
     llm,
     graph=graph,
     cypher_prompt=cypher_generation_prompt,
-    verbose=True,
+    verbose=True,#Ativa logs detalhados para depuração.
     enhanced_schema=True, #Mais precisão
 )
 
+# A pergunta é passada para o modelo de linguagem.
+# O modelo gera um comando Cypher
+# O comando é executado no Neo4j.
+# Os resultados são retornados e exibidos no terminal.
 def run_cypher(q):
     return cypher_chain.invoke({"query": q})
 
+#Loop de interação com o usuário
 while True:
     q = input("> ")
     print(run_cypher(q))
